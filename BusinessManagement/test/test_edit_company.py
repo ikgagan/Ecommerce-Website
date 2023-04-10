7380f9d918d71fb2dff643f7c00d97286e74fe19
Gagan Indukala Krishna Murthy <123420429+ikgagan@users.noreply.github.com>
2023-04-10 01:00:01 -0400

    company add and edit completed - test cases working

--- BusinessManagement/test/test_edit_company.py
+++ BusinessManagement/test/test_edit_company.py
@@ -43,6 +43,7 @@ def test_edit_company(client):
         "city": "Testville",
         "zip":"00000"
     }, follow_redirects=True )
+    # print(resp.data)
     assert resp.status_code == 200
     resp = client.get("/company/edit?id=-1", follow_redirects=True )
     # print(resp.data)
@@ -50,7 +51,7 @@ def test_edit_company(client):
     soup = BeautifulSoup(resp.data, "html.parser")
     form = soup.form
     ele = form.select("[name='city']")[0]
-    print(ele)
+    # print(ele)
     # TODO the dropdown option doesn't need to exist, it just needs to
     # be set on the select element so this is fine
     assert ele.get("value") == 'Testville'
--- BusinessManagement/views/company.py
+++ BusinessManagement/views/company.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, redirect, render_template, request, flash, url_for
 from sql.db import DB
+from bs4 import BeautifulSoup
 company = Blueprint('company', __name__, url_prefix='/company')
 
 @company.route("/search", methods=["GET"])
+# function for lising and searching companies
+# Gagan Indukala Krishna Murthy - gi36 - 5th April 
 def search():
     rows = []
     # DO NOT DELETE PROVIDED COMMENTS
@@ -49,12 +52,8 @@ def search():
     print("args", args)
     try:
         result = DB.selectAll(query, *args)
-        print("1")
-        print(f"result {result.rows}")
-        print(f"errorrrrrrrrrrrrr{result.status}")
         if result.status:
             rows = result.rows
-            print(f"rows {rows}")
     except Exception as e:
         print(f"check error{e}")
         # TODO search-9 make message user friendly
@@ -67,7 +66,10 @@ def search():
     return render_template("list_companies.html", rows=rows, allowed_columns=allowed_columns)
 
 @company.route("/add", methods=["GET","POST"])
+# Gagan Indukala Krishna Murthu - gi36- April 9th
+# function for adding companies
 def add():
+    input = request.form
     if request.method == "POST":
         # TODO add-1 retrieve form data for name, address, city, state, country, zip, website
         # TODO add-2 name is required (flash proper error message)
@@ -82,32 +84,50 @@ def add():
         # TODO add-7 website is not required
         # TODO add-8 zipcode is required (flash proper error message)
         # note: call zip variable zipcode as zip is a built in function it could lead to issues
-
+        # Gagan Indukala Krishna Murthu - gi36- April 9th
+        form_value = {}
         has_error = False # use this to control whether or not an insert occurs
-        
+
+        form_value["name"] = input.getlist('name')
+        form_value["address"] = input.getlist('address')
+        form_value["city"] = input.getlist('city')
+        form_value["country"] = request.form.get("country")
+        form_value["state"] = request.form.get("state")
+        form_value["zip_code"] = input.getlist('zip')
+        form_value["website"] = input.getlist('website')
+
+        for k,v in form_value.items():
+            if k != "website" and not v:
+                flash(f"{k} is a mandatory field", "danger")
+                has_error = True
+            elif k == "website" and not v:
+                form_value[k] = None
 
         if not has_error:
             try:
+                # Gagan Indukala Krishna Murthu - gi36- April 9th
                 result = DB.insertOne("""
-                INSERT INTO ...
-                ...
-                VALUES
-                ...
-                """, ...) # <-- TODO add-8 add query and add arguments
+                INSERT INTO IS601_MP3_Companies (name, address, city, country,
+                state, zip, website)
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+                """, *form_value.values()) # <-- TODO add-8 add query and add arguments
                 if result.status:
                     flash("Added Company", "success")
             except Exception as e:
                 # TODO add-9 make message user friendly
-                flash(str(e), "danger")
-        
-    return render_template("add_company.html")
+                # Gagan Indukala Krishna Murthu - gi36- April 9th
+                flash(f"An unexpected error while trying to edit company: {e}", "danger")
+    return render_template("add_company.html", input=input)
 
 @company.route("/edit", methods=["GET", "POST"])
+# function for editing companies
 def edit():
     # TODO edit-1 request args id is required (flash proper error message)
-    id = False
+    # Gagan Indukala Krishna Murthu - gi36- April 9th 
+    input = request.form
+    id = request.args.get("id")
     if not id: # TODO update this for TODO edit-1
-        pass
+        flash("Company ID is not available ", "danger")
     else:
         if request.method == "POST":
             data = {"id": id} # use this as needed, can convert to tuple if necessary
@@ -123,42 +143,66 @@ def edit():
             # hint see geography.py and pycountry documentation
             # TODO edit-7 website is not required
             # TODO edit-8 zipcode is required (flash proper error message)
-            
+            # Gagan Indukala Krishna Murthu - gi36- April 9th 
             # note: call zip variable zipcode as zip is a built in function it could lead to issues
             # populate data dict with mappings
+            form_value={}
             has_error = False # use this to control whether or not an insert occurs
+            # Gagan Indukala Krishna Murthu - gi36- April 9th 
+            form_value["name"] = input.getlist('name')
+            form_value["address"] = input.getlist('address')
+            form_value["city"] = input.getlist('city')
+            form_value["country"] = request.form.get("country")
+            form_value["state"] = request.form.get("state")
+            form_value["zip_code"] = input.getlist('zip')
+            form_value["website"] = input.getlist('website')
+            form_value["id"] = request.args.get('id')
+            # Gagan Indukala Krishna Murthu - gi36- April 9th 
+            for k,v in form_value.items():
+                if k != "website" and not v[0]:
+                    flash(f"{k} is a mandatory field", "danger")
+                    has_error = True
+                elif k == "website" and not v:
+                    form_value[k] = None
 
-            
             if not has_error:
                 try:
                     # TODO edit-9 fill in proper update query
                     # name, address, city, state, country, zip, website
-                    result = DB.update("""
-                    UPDATE ...
-                    SET
-                    ...
-                    """, data)
+                    # Gagan Indukala Krishna Murthu - gi36- April 9th 
+                    result = DB.update(
+                    """
+                    UPDATE IS601_MP3_Companies 
+                    SET name=%s, address=%s, city=%s, country=%s, state=%s,
+                    zip=%s, website=%s
+                    WHERE id=%s
+                    """, *form_value.values())
                     if result.status:
-                        print("updated record")
+                        print("updated")
                         flash("Updated record", "success")
                 except Exception as e:
                     # TODO edit-10 make this user-friendly
-                    print(f"{e}")
-                    flash(str(e), "danger")
-        row = {}
+                    # Gagan Indukala Krishna Murthu - gi36- April 9th 
+                    import traceback as tb
+                    print(tb.format_exc())
+                    flash(f"An unexpected error while trying to edit company: {e}", "danger")
+        row = None
         try:
             # TODO edit-11 fetch the updated data
-            result = DB.selectOne("SELECT ... FROM ... WHERE ...", id)
+            # Gagan Indukala Krishna Murthu - gi36- April 9th 
+            result = DB.selectOne("""SELECT * FROM IS601_MP3_Companies WHERE id=%s""", id)
             if result.status:
                 row = result.row
-                
         except Exception as e:
             # TODO edit-12 make this user-friendly
-            flash(str(e), "danger")
+            # Gagan Indukala Krishna Murthu - gi36- April 9th 
+            flash(f"An unexpected error while trying to fetch the updated company: {e}", "danger")
     # TODO edit-13 pass the company data to the render template
-    return render_template("edit_company.html", ...)
+    # Gagan Indukala Krishna Murthu - gi36- April 9th 
+    return render_template("edit_company.html", row=row, country=row.get("country"), state=row.get("state"))
 
 @company.route("/delete", methods=["GET"])
+# function for deleting companies
 def delete():
     # TODO delete-1 delete company by id (unallocate any employees see delete-5)
     # TODO delete-2 redirect to company search
