5a8da5c2d04efe91f9834115b361f303e973a08b
Gagan Indukala Krishna Murthy <123420429+ikgagan@users.noreply.github.com>
2023-04-14 01:57:32 -0400

    flash message changes

--- Project/auth/auth.py
+++ Project/auth/auth.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, render_template, flash, redirect, url_for,current_app, session
 from auth.forms import LoginForm, ProfileForm, RegisterForm
 from sql.db import DB
-
 from flask_login import login_user, login_required, logout_user, current_user
 from auth.models import User
 from flask_bcrypt import Bcrypt
+import re
 
 bcrypt = Bcrypt()
 
@@ -13,6 +13,7 @@ from flask_principal import Identity, AnonymousIdentity, \
 from email_validator import validate_email
 
 auth = Blueprint('auth', __name__, url_prefix='/',template_folder='templates')
+
 
 
 @auth.route("/register", methods=["GET","POST"])
@@ -30,7 +31,11 @@ def register():
             if result.status:
                 flash("Successfully registered","success")
         except Exception as e:
-            flash(str(e), "danger")
+            r = re.match(".*IS601_Users.(\w+)", str(e))
+            if r:
+                flash(f"The chosen {r.group(1   )} is not available", "warning")
+            else:
+                flash("Unknown error occurred, please try again", "danger")
     return render_template("register.html", form=form)
 
 @auth.route("/login", methods=["GET", "POST"])
@@ -154,7 +159,11 @@ def profile():
                 if result.status:
                     flash("Saved profile", "success")
             except Exception as e:
-                flash(e, "danger")
+                r = re.match(".*IS601_Users.(\w+)", str(e))
+            if r:
+                flash(f"The chosen {r.group(1   )} is not available", "warning")
+            else:
+                flash("Unknown error occurred, please try again", "danger")
     try:
         # get latest info if anything changed
         result = DB.selectOne("SELECT id, email, username FROM IS601_Users where id = %s", user_id)
