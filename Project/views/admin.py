f2655f23326bc2d52c95e99d54f9b090ebc21004
Gagan Indukala Krishna Murthy <123420429+ikgagan@users.noreply.github.com>
2023-04-22 19:34:44 -0400

    fixed a bug for item edit

--- Project/views/admin.py
+++ Project/views/admin.py
@@ -11,41 +11,35 @@ admin = Blueprint('admin', __name__, url_prefix='/',template_folder='templates')
 @admin.route("/admin/item", methods=["GET","POST"])
 @admin_permission.require(http_exception=403)
 def item():
-    # Gagan Indukala Krishna Murthy - gi36 - April 21th
     form = ItemForm()
     id = request.args.get("id", form.id.data or None)
     type = "Edit" if id else "Create"
-    if id:
-        result = DB.selectOne("SELECT id, name, description, stock, unit_price, image FROM IS601_S_Items WHERE id = %s", id)
-        form.process(MultiDict(result.row))
     if form.validate_on_submit():
-        visibility = True if int(form.stock.data) > 0 else False
-        if form.id.data: 
+        if form.id.data: # it's an update
             try:
-                result = DB.update("UPDATE IS601_S_Items set name = %s, description = %s, stock = %s, unit_price = %s, image=%s, visbility=%s WHERE id = %s",
-                form.name.data, form.description.data, form.stock.data, form.unit_price.data, form.image.data, visibility, form.id.data)
+                result = DB.update("UPDATE IS601_S_Items set name = %s, description = %s, stock = %s, unit_price = %s, image=%s WHERE id = %s",
+                form.name.data, form.description.data, form.stock.data, form.unit_price.data, form.image.data, form.id.data)
                 if result.status:
                     flash(f"Updated {form.name.data}", "success")
             except Exception as e:
                 print("Error updating item", e)
                 flash(f"Error updating item {form.name.data}", "danger")
-        else: 
+        else: # it's a create
             try:
-                result = DB.update("""INSERT INTO IS601_S_Items (name, description, stock, unit_price, image, visibility) 
-                VALUES (%s,%s,%s,%s,%s,%s)""",
-                form.name.data, form.description.data, form.stock.data, form.unit_price.data, form.image.data, visibility)
+                result = DB.update("""INSERT INTO IS601_S_Items (name, description, stock, unit_price, image) 
+                VALUES (%s,%s,%s,%s,%s)""",
+                form.name.data, form.description.data, form.stock.data, form.unit_price.data, form.image.data)
                 if result.status:
                     flash(f"Created {form.name.data}", "success")
                     form = ItemForm() # clear form
             except Exception as e:
                 print("Error creating item", e)
                 flash(f"Error creating item {form.name.data}", "danger")
-
     if id:
         try:
             result = DB.selectOne("SELECT id, name, description, stock, unit_price, image FROM IS601_S_Items WHERE id = %s", id)
             if result.status and result.row:
-                form.process(MultiDict(result.row))
+                    form.process(MultiDict(result.row))
         except Exception as e:
             print("Error fetching item", e)
             flash("Item not found", "danger")
--- Project/views/forms.py
+++ Project/views/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, HiddenField, TextAreaField, IntegerField, URLField, SubmitField
+from wtforms import StringField, HiddenField, TextAreaField, IntegerField, URLField, SubmitField, FloatField
 from wtforms.validators import DataRequired, Length, Optional, NumberRange
 # Gagan Indukala Krishna Murthy - gi36 - April 21th
 class ItemForm(FlaskForm):
@@ -7,6 +7,6 @@ class ItemForm(FlaskForm):
     name = StringField("name", validators=[DataRequired(), Length(max=30)])
     description = TextAreaField("description", validators=[DataRequired()])
     stock = IntegerField("stock", validators=[NumberRange(min=0)])
-    unit_price = IntegerField("unit_price", validators=[NumberRange(min=0)])
+    unit_price = FloatField("unit_price", validators=[NumberRange(min=0)])
     image = URLField("image", validators=[Optional()])
     submit = SubmitField("Save")
